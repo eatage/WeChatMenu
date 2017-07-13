09308b3c0860735c7239d7a6ebf91aabf310b3db
eatage <xuxinbo@outlook.com>
2017-07-13 14:05:49 +0800

    修复Bug

--- WeChatMenu/WeChatMenu.ashx.cs
+++ WeChatMenu/WeChatMenu.ashx.cs
@@ -19,16 +19,13 @@ namespace WeChatMenu
             string ls_response = "";
             switch (action)
             {
-                case "GETMENU":
+                case "GETMENU"://获取现有菜单
                     ls_response = GetMenu(appid, appsecret);
                     break;
-                case "UPDATEMENU":
+                case "UPDATEMENU"://更新菜单
                     ls_response = UpdateMenu(appid, appsecret, json);
                     break;
-                case "BAKMENU":
-                    ls_response = BakMenu(json);
-                    break;
-                case "DELETEMENU":
+                case "DELETEMENU"://删除菜单
                     ls_response = DelMenu(appid, appsecret);
                     break;
                 default:
@@ -45,9 +42,9 @@ namespace WeChatMenu
         {
             if (string.IsNullOrEmpty(appid) || string.IsNullOrEmpty(appsecret))
                 return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不能为空\" }";
-            string access_token = Get_Access_token(appid, appsecret);
-            if (string.IsNullOrEmpty(access_token))
-                return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不对，请检查后再操作\" }";
+            string access_token = "";
+            if (!Get_Access_token(appid, appsecret, out access_token))
+                return "{\"errcode\":-2,\"errmsg\":\"" + access_token + "\" }";
             string url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/get?access_token={0}", access_token);
             string ls_rc = HttpGet(url);//获取当前菜单
             JObject json = JObject.Parse(ls_rc);
@@ -60,9 +57,9 @@ namespace WeChatMenu
         {
             if (string.IsNullOrEmpty(appid) || string.IsNullOrEmpty(appsecret))
                 return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不能为空\" }";
-            string access_token = Get_Access_token(appid, appsecret);
-            if (string.IsNullOrEmpty(access_token))
-                return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不对，请检查后再操作\" }";
+            string access_token = "";
+            if (!Get_Access_token(appid, appsecret, out access_token))
+                return "{\"errcode\":-2,\"errmsg\":\"" + access_token + "\" }";
             if (string.IsNullOrEmpty(json))
                 return "{\"errcode\":-2,\"errmsg\":\"json不能为空\" }";
             string url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}", access_token);
@@ -74,28 +71,13 @@ namespace WeChatMenu
             }
             catch (System.Exception ex) { return "{\"errcode\":-2,\"errmsg\":\"" + ex.Message + "\" }"; }
             string ls_rc = HttpPost(url, json);
+            if (ls_rc.Substring(0, 2) == "-1")
+                return "{\"errcode\":-2,\"errmsg\":\"菜单更新失败<br>" + HTTPERROR + "\" }";
             JObject jo = JObject.Parse(ls_rc);
             if (jo["errmsg"].ToString() == "ok")
                 return "{\"errcode\":0,\"errmsg\":\"菜单更新成功\" }";
             else
                 return "{\"errcode\":-2,\"errmsg\":\"菜单更新失败<br>" + jo["errmsg"].ToString() + "\" }";
-        }
-        /// <summary>
-        /// 备份菜单
-        /// <para>此方法是为了格式化菜单</para>
-        /// </summary>
-        private string BakMenu(string json)
-        {
-            if (string.IsNullOrEmpty(json))
-                return "{\"errcode\":-2,\"errmsg\":\"json不能为空\" }";
-            try
-            {
-                string ls_json = json;
-                JObject _json = JObject.Parse(ls_json);
-                json = _json.ToString();
-            }
-            catch (System.Exception ex) { return "{\"errcode\":-2,\"errmsg\":\"" + ex.Message + "\" }"; }
-            return json;
         }
         /// <summary>
         /// 删除菜单
@@ -104,9 +86,9 @@ namespace WeChatMenu
         {
             if (string.IsNullOrEmpty(appid) || string.IsNullOrEmpty(appsecret))
                 return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不能为空\" }";
-            string access_token = Get_Access_token(appid, appsecret);
-            if (string.IsNullOrEmpty(access_token))
-                return "{\"errcode\":-2,\"errmsg\":\"Appid或AppSecret不对，请检查后再操作\" }";
+            string access_token = "";
+            if (!Get_Access_token(appid, appsecret, out access_token))
+                return "{\"errcode\":-2,\"errmsg\":\"" + access_token + "\" }";
             string url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/delete?access_token={0}", access_token);
             string ls_Message = HttpGet(url);//获取当前菜单
             JObject json = JObject.Parse(ls_Message);
@@ -117,31 +99,39 @@ namespace WeChatMenu
 
         }
         /// <summary>
+        /// 执行HTTP访问返回错误信息
+        /// </summary>
+        string HTTPERROR = string.Empty;
+        /// <summary>
         /// 获取access_token
         /// <para>这里不校验是否正确</para>
         /// <para>如果Session有效期间在其他地方更新了access_token,请更换浏览器</para>
         /// </summary>
         /// <param name="appid">appid</param>
         /// <param name="secret">appsecret</param>
-        private string Get_Access_token(string appid, string appsecret)
+        private bool Get_Access_token(string appid, string appsecret, out string access_token)
         {
-            if (System.Web.HttpContext.Current.Session["appid"] != null) return System.Web.HttpContext.Current.Session["appid"].ToString();
+            if (System.Web.HttpContext.Current.Session["appid"] != null)
+            { access_token = System.Web.HttpContext.Current.Session["appid"].ToString(); return true; }
             string ls_appid = appid.Replace(" ", "");
             string ls_secret = appsecret.Replace(" ", "");
-            string access_token = "";
+            access_token = "";
             string url = string.Format("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}", ls_appid, ls_secret);
             string ls_rc = HttpGet(url);
+            if (ls_rc.Substring(0, 2) == "-1")
+            { access_token = "无法连接到远程服务器，请检查网络设置"; return false; }
             JObject ja = JObject.Parse(ls_rc);
             try { access_token = ja["access_token"].ToString(); }
-            catch { return ""; }
+            catch { access_token = ls_rc; return false; }
             System.Web.HttpContext.Current.Session["appid"] = access_token;
-            return access_token;
+            return true;
         }
         /// <summary>
         /// Get方法获取网页信息
         /// </summary>
-        public static string HttpGet(string as_url)
+        public string HttpGet(string as_url)
         {
+            HTTPERROR = string.Empty;
             System.Net.WebClient webClient = new System.Net.WebClient();
             webClient.Headers.Add("Accept", "*/*");
             webClient.Headers.Add("Accept-Language", "zh-cn");
@@ -154,10 +144,12 @@ namespace WeChatMenu
             }
             catch (System.Net.WebException ex)
             {
+                HTTPERROR = ex.ToString();
                 return "-1," + ex.ToString();
             }
             catch (System.Exception ex)
             {
+                HTTPERROR = ex.ToString();
                 return "-1," + ex.ToString();
             }
             finally
@@ -171,6 +163,7 @@ namespace WeChatMenu
         /// </summary>
         public string HttpPost(string Url, string Params)
         {
+            HTTPERROR = string.Empty;
             System.Net.WebClient webClient = new System.Net.WebClient();
             webClient.Headers.Add("Accept", "*/*");
             webClient.Headers.Add("Accept-Language", "zh-cn");
@@ -185,6 +178,7 @@ namespace WeChatMenu
             }
             catch (System.Exception Exce)
             {
+                HTTPERROR = Exce.ToString();
                 return "-1," + Exce.ToString();
             }
             finally
--- WeChatMenu/js/wechatmenu.js
+++ WeChatMenu/js/wechatmenu.js
@@ -2,7 +2,7 @@
     //从缓存写入数据
     var appid = localStorage.getItem('appid');
     var appsecret = localStorage.getItem('appsecret');
-    if (appid != null && appsecret != null) {
+    if (appid !== null && appsecret !== null) {
         $("#txt_appid").val(appid);
         $("#txt_appsecret").val(appsecret);
     }
@@ -14,7 +14,7 @@
                 '<td class="td_shengru" style="text-align: center;">第三列</td></tr></thead>';
     tablehtml += '<tbody>';
     for (var i = 0; i < 6; i++) {
-        if (i == 0) tablehtml += '<tr><td class="td_shengru">主菜单按钮</td>';
+        if (i === 0) tablehtml += '<tr><td class="td_shengru">主菜单按钮</td>';
         else tablehtml += '<tr><td class="td_shengru">二级菜单No.' + i + '</td>';
         for (var j = 0; j < 3; j++) {
             var id = (j + 1) + '';
@@ -36,10 +36,10 @@
     tablehtml += '</table>';
     document.getElementById('menutable').innerHTML = tablehtml;
     //为所有select添加Option
-    for (var i = 0; i < 3; i++) {
-        Typemenu("DDL_Top" + (i + 1));
-        for (var j = 0; j < 5; j++) {
-            Typemenu("DDL_Top" + (i + 1) + (j + 1));
+    for (var k = 0; k < 3; k++) {
+        Typemenu("DDL_Top" + (k + 1));
+        for (var m = 0; m < 5; m++) {
+            Typemenu("DDL_Top" + (k + 1) + (m + 1));
         }
     }
     chengesel('DDL_Top1', 1); chengesel('DDL_Top2', 2); chengesel('DDL_Top3', 3);
@@ -86,11 +86,11 @@ function Typemenu(id) {
 function checkAppidAndAppsecret() {
     var appid = $("#txt_appid").val();
     var appsecret = $("#txt_appsecret").val();
-    if (appid == "") {
+    if (appid === "") {
         dialogMsg('请填appid');
         return false;
     }
-    if (appsecret == "") {
+    if (appsecret === "") {
         dialogMsg('请填appsecret');
         return false;
     }
@@ -98,7 +98,7 @@ function checkAppidAndAppsecret() {
     localStorage.setItem('appsecret', appsecret);
     return true;
 }
-//从备份数据更新
+//打开从备份数据更新窗口
 function checkback() {
     if (checkAppidAndAppsecret()) {
         $("#txt_bak").val('');
@@ -109,7 +109,7 @@ function checkback() {
 //从备份数据更新
 function importmenu() {
     var menu = $("#txt_bak").val();
-    if (menu == "") {
+    if (menu === "") {
         dialogMsg('请填入备份的菜单数据');
         return;
     }
@@ -126,22 +126,22 @@ function getmenu() {
             async: false,
             success: function (data) {
                 var json = eval("(" + data + ")");
-                if (json.errmsg != undefined) {
+                if (json.errmsg !== undefined) {
                     dialogMsg(json.errmsg, 4000, 'error');
                     return;
                 }
-                if (json.menu.button[0] != undefined) {
+                if (json.menu.button[0] !== undefined) {
                     for (var i = 0; i < 3; i++) {
                         var type = json.menu.button[i].type;
                         var name = json.menu.button[i].name;
                         var keyOrUrl = '';
-                        if (json.menu.button[i].key == undefined) keyOrUrl = json.menu.button[i].url;
+                        if (json.menu.button[i].key === undefined) keyOrUrl = json.menu.button[i].url;
                         else keyOrUrl = json.menu.button[i].key;
                         $("#txt_Top" + (i + 1)).val(name);
                         $("#DDL_Top" + (i + 1)).val(type);
                         $("#txt_Key" + (i + 1)).val(keyOrUrl);
                         //没有子菜单
-                        if (json.menu.button[i].sub_button.length == 0) {
+                        if (json.menu.button[i].sub_button.length === 0) {
                             continue;
                         } else {
                             //有子菜单
@@ -150,7 +150,7 @@ function getmenu() {
                                 var _type = json.menu.button[i].sub_button[j].type;
                                 var _name = json.menu.button[i].sub_button[j].name;
                                 var _keyOrUrl = '';
-                                if (json.menu.button[i].sub_button[j].key == undefined) keyOrUrl = json.menu.button[i].sub_button[j].url;
+                                if (json.menu.button[i].sub_button[j].key === undefined) keyOrUrl = json.menu.button[i].sub_button[j].url;
                                 else keyOrUrl = json.menu.button[i].sub_button[j].key;
                                 $("#txt_Top" + (i + 1) + (j + 1)).val(_name);
                                 $("#DDL_Top" + (i + 1) + (j + 1)).val(_type);
@@ -175,22 +175,25 @@ function getJson() {
         var name = $("#txt_Top" + (i + 1)).val();
         var type = $("#DDL_Top" + (i + 1)).val();
         var key = $("#txt_Key" + (i + 1)).val();
-        if (name == "" && i == 0) {
+        if (name === "" && i === 0) {
             dialogMsg('第一个一级菜单名称不允许为空', 4000, 'error');
             return "";
         }
+        else if (name === "") {
+            continue;
+        }
         json += '{"name": "' + name + '",';
-        if (type != "") {
-            if (key == "") {
+        if (type !== "") {
+            if (key === "") {
                 dialogMsg('第' + (i + 1) + '个一级菜单设置错误<br>已经选择了类型的情况下,key/url不允许为空', 4000, 'error');
                 return "";
             }
-            if (name == "") {
+            if (name === "") {
                 json = json.substr(0, json.length - 1);
                 continue;
             }
             json += '"type": "' + type + '",';
-            if (type == "view")
+            if (type === "view")
                 json += '"url": "' + key + '"},';
             else
                 json += '"key": "' + key + '"},';
@@ -201,17 +204,17 @@ function getJson() {
                 var _name = $("#txt_Top" + (i + 1) + (j + 1)).val();
                 var _type = $("#DDL_Top" + (i + 1) + (j + 1)).val();
                 var _key = $("#txt_Key" + (i + 1) + (j + 1)).val();
-                if ((_name == "" || _type == "" || _key == "") && j == 0) {
+                if ((_name === "" || _type === "" || _key === "") && j === 0 && name !== "") {
                     dialogMsg('第' + (i + 1) + '个一级菜单下的第' + (j + 1) + '个二级菜单设置错误<br>对应一级菜单没有选择类型的情况下,第一个二级菜单必须有效', 4000, 'error');
                     return "";
                 }
-                if (_name == "" || _type == "" || _key == "") {
+                if (_name === "" || _type === "" || _key === "") {
                     json = json.substr(0, json.length - 1);
                     break;
                 }
                 json += '{"type": "' + _type + '",';
                 json += '"name": "' + _name + '",';
-                if (_type == "view")
+                if (_type === "view")
                     json += '"url": "' + _key + '"},';
                 else
                     json += '"key": "' + _key + '"},';
@@ -228,9 +231,9 @@ function getJson() {
 function updatemenu(menu) {
     if (checkAppidAndAppsecret()) {
         var _json = "";
-        if (menu == undefined) _json = getJson();
+        if (menu === undefined) _json = getJson();
         else _json = menu;
-        if (_json != "") {
+        if (_json !== "") {
             $.ajax({
                 type: 'post',
                 dataType: "text",
@@ -252,22 +255,12 @@ function updatemenu(menu) {
 function bakmenu() {
     if (checkAppidAndAppsecret()) {
         var _json = getJson();
-        if (_json != "") {
-            $.ajax({
-                type: 'post',
-                dataType: "text",
-                url: "WeChatMenu.ashx",
-                data: { action: 'BAKMENU', appid: $("#txt_appid").val(), appsecret: $("#txt_appsecret").val(), json: _json },
-                async: false,
-                success: function (data) {
-                    $("#txt_bak").val(data);
-                    $("#btn_import").hide();
-                    $('#menubak').fadeIn(200);
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    dialogMsg(XMLHttpRequest.status + '<br>' + XMLHttpRequest.readyState + '<br>' + textStatus, 4000, 'error');
-                }
-            });
+        if (_json !== "") {
+            //格式化json
+            _json = formatJson(_json);
+            //csv格式需要在文件内容前加BOM头\ufeff 否则Excel打开将乱码
+            var blob = new Blob([_json], { type: 'text/text,charset=UTF-8' });
+            openDownloadDialog(blob, (new Date()).Format("yyyyMMddhhmmss") + '导出菜单.txt');
         }
     }
 }
@@ -308,11 +301,11 @@ function reducedStyle(x) {
 }
 //当一级菜单 绑定了事件时 隐藏对应的二级菜单
 function chengesel(sel, num) {
-    if (sel != "DDL_Top1" && sel != "DDL_Top2" && sel != "DDL_Top3") {
+    if (sel !== "DDL_Top1" && sel !== "DDL_Top2" && sel !== "DDL_Top3") {
         return;
     }
     var value = $("#" + sel).val();
-    if (value == "") {
+    if (value === "") {
         $(".top" + num).fadeIn(200);
         $("#txt_Key" + num).attr("disabled", "disabled")
     }
@@ -323,10 +316,10 @@ function chengesel(sel, num) {
 }
 //提示信息
 function dialogMsg(msg, time, type) {
-    if (time == undefined) {
+    if (time === undefined) {
         time = 4000;
     }
-    if (type == undefined) {
+    if (type === undefined) {
         type = 'success';
     }
     MsgTips(time, msg, 300, type);
@@ -352,4 +345,49 @@ function MsgTips(timeOut, msg, speed, type) {
         $(".tip_container").remove();
     }, timeOut);
     $("#tip_container" + bid).css("left", ($(window).width() - $("#tip_container" + bid).width()) / 2);
+}
+/**
+*通用的打开下载对话框方法，没有测试过具体兼容性
+*@method 下载
+*@param {string}url 下载地址，也可以是一个blob对象，必选
+*@param {string}saveName 保存文件名，可选
+ */
+function openDownloadDialog(url, saveName) {
+    if (typeof url === 'object' && url instanceof Blob) {
+        url = URL.createObjectURL(url); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+    var event;
+    if (window.MouseEvent) event = new MouseEvent('click');
+    else {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+    aLink.dispatchEvent(event);
+}
+/**
+ * 对Date的扩展，将 Date 转化为指定格式的String
+ * 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+ * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+ *@method 格式化日期
+ *@param {Date}fmt 日期
+ *@param {object}function 回调
+ *@return {string}格式化的日期
+ */
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
 }
